8e3c77e04c3dd030971c149f9ef8e7168d917883
Farras Adhani Zayn <Farras.FF6@gmail.com>
2026-06-08 16:05:54 +0700

    chore(config): allow admin.zapp.web.id as next/image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['dev.zapp.web.id'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'admin.zapp.web.id',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
